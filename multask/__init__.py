2296b8d10700a16dda44f53cc0289835d831eb27
ludwigax <yuzeliu@gmail.com>
2025-09-05 04:53:17 +0800

    del depr 0905

--- multask/__init__.py
+++ multask/__init__.py
@@ -13,16 +13,5 @@ from .core import (
     CircuitBreakerError
 )
 
-# Legacy compatibility (deprecated)
-from .req.async_request import AsyncCake
-from .req.thread_request import ThreadedCake
-
 # API interfaces with graceful error handling
 from . import apis
-
-# Legacy easy-use functions (deprecated, use apis instead)
-from .easy_use import (
-    batch_query_openai,
-    oai_price_calculator,
-    batch_query_crossref
-)
